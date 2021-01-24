56fd3b061b6abc3799e20f337a6c9f6c566c5a61
Jake Meyers <jakejake92@yahoo.com>
2021-01-24 13:17:08 -0700

    added favoriates functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	const [drinkArray, setDrinkArray] = useState([]);
+	const [favoriates, setFavoriates] = useState([]);
+
+	const createDrinkFavoriate = () => {
+		console.log("working");
+	};
 
 	const searchFormSubmit = async (term) => {
 		await fetch(
@@ -21,7 +26,10 @@ function App() {
 				<Switch>
 					<Route path='/' exact>
 						<Search searchFormSubmit={searchFormSubmit} />
-						<DrinkList drinks={drinkArray} />
+						<DrinkList
+							drinks={drinkArray}
+							createDrinkFavoriate={createDrinkFavoriate}
+						/>
 					</Route>
 					<Route path='/cocktail/:name' component={Drink}></Route>
 				</Switch>
--- src/Drink/Drink.js
+++ src/Drink/Drink.js
@@ -5,6 +5,10 @@ const Drink = (match) => {
 	const [name, setName] = useState({});
 	const id = match.match.params.name;
 
+	const heartClick = () => {
+		document.querySelector(".fa-heart").classList.toggle("clicked");
+	};
+
 	const fetchItem = async () => {
 		await fetch(
 			`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
@@ -16,6 +20,7 @@ const Drink = (match) => {
 
 	useEffect(() => {
 		fetchItem();
+		console.log(name);
 	}, []);
 
 	if (name === {} || id === "") {
@@ -23,35 +28,44 @@ const Drink = (match) => {
 	} else {
 		return (
 			<div key={id} className='drank'>
-				<h2>{name.strDrink}</h2>
-				<p>Ingridents:</p>
+				<div className='drankContainer'>
+					<i class='fas fa-heart' onClick={heartClick}></i>
+					<img src={name.strDrinkThumb} alt='picture of drink' />
+					<h2>{name.strDrink}</h2>
 
-				<div className='drankIngDiv'>
-					<span>{name.strIngredient1 ? name.strIngredient1 : " "}</span>
-					<span>{name.strMeasure1 ? name.strMeasure1 : " "} </span>
-				</div>
-				<div className='drankIngDiv'>
-					<span>{name.strIngredient2 ? name.strIngredient2 : " "}</span>
-					<span>{name.strMeasure2 ?  name.strMeasure2 : " "} </span>
-				</div>
-				<div className='drankIngDiv'>
-					<span>{name.strIngredient3 ? name.strIngredient3 : " "}</span>
-					<span>{name.strMeasure3 ? name.strMeasure3 : " "} </span>
-				</div>
-				<div className='drankIngDiv'>
-					<span>{name.strIngredient4 ? name.strIngredient4 : " "}</span>
-					<span>{name.strMeasure4 ? name.strMeasure4 : " "} </span>
-				</div>
-				<div className='drankIngDiv'>
-					<span>{name.strIngredient5 ? name.strIngredient5 : " "}</span>
-					<span>{name.strMeasure5 ? name.strMeasure5 : " "} </span>
-				</div>
-				<div className='drankIngDiv'>
-					<span>{name.strIngredient6 ? name.strIngredient6 : " "}</span>
-					<span>{name.strMeasure6 ? name.strMeasure6 : " "} </span>
-				</div>
+					<p>
+						<strong>Ingredients:</strong>
+					</p>
 
-				<p>Instructions: {name.strInstructions}</p>
+					<div className='drankIngDiv'>
+						<span>{name.strIngredient1 ? name.strIngredient1 : " "}</span>
+						<span>{name.strMeasure1 ? name.strMeasure1 : " "} </span>
+					</div>
+					<div className='drankIngDiv'>
+						<span>{name.strIngredient2 ? name.strIngredient2 : " "}</span>
+						<span>{name.strMeasure2 ? name.strMeasure2 : " "} </span>
+					</div>
+					<div className='drankIngDiv'>
+						<span>{name.strIngredient3 ? name.strIngredient3 : " "}</span>
+						<span>{name.strMeasure3 ? name.strMeasure3 : " "} </span>
+					</div>
+					<div className='drankIngDiv'>
+						<span>{name.strIngredient4 ? name.strIngredient4 : " "}</span>
+						<span>{name.strMeasure4 ? name.strMeasure4 : " "} </span>
+					</div>
+					<div className='drankIngDiv'>
+						<span>{name.strIngredient5 ? name.strIngredient5 : " "}</span>
+						<span>{name.strMeasure5 ? name.strMeasure5 : " "} </span>
+					</div>
+					<div className='drankIngDiv'>
+						<span>{name.strIngredient6 ? name.strIngredient6 : " "}</span>
+						<span>{name.strMeasure6 ? name.strMeasure6 : " "} </span>
+					</div>
+
+					<p>
+						<strong>Insturctions:</strong> {name.strInstructions}
+					</p>
+				</div>
 			</div>
 		);
 	}

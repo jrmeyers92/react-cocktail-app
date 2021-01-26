496919a6634124d3ecd82c40b94a589093442bb5
Jake Meyers <jakejake92@yahoo.com>
2021-01-26 16:53:53 -0700

    fixed styling for drink component

--- src/Drink/Drink.js
+++ src/Drink/Drink.js
@@ -30,41 +30,45 @@ const Drink = (match) => {
 			<div key={id} className='drank'>
 				<div className='drankContainer'>
 					<i class='fas fa-heart' onClick={heartClick}></i>
-					<img src={name.strDrinkThumb} alt='picture of drink' />
-					<h2>{name.strDrink}</h2>
+					<div className='drank__left'>
+						<img src={name.strDrinkThumb} alt='picture of drink' />
+					</div>
+					<div className='drank__right'>
+						<h2>{name.strDrink}</h2>
 
-					<p>
-						<strong>Ingredients:</strong>
-					</p>
+						<p>
+							<strong>Ingredients:</strong>
+						</p>
 
-					<div className='drankIngDiv'>
-						<span>{name.strIngredient1 ? name.strIngredient1 : " "}</span>
-						<span>{name.strMeasure1 ? name.strMeasure1 : " "} </span>
-					</div>
-					<div className='drankIngDiv'>
-						<span>{name.strIngredient2 ? name.strIngredient2 : " "}</span>
-						<span>{name.strMeasure2 ? name.strMeasure2 : " "} </span>
-					</div>
-					<div className='drankIngDiv'>
-						<span>{name.strIngredient3 ? name.strIngredient3 : " "}</span>
-						<span>{name.strMeasure3 ? name.strMeasure3 : " "} </span>
-					</div>
-					<div className='drankIngDiv'>
-						<span>{name.strIngredient4 ? name.strIngredient4 : " "}</span>
-						<span>{name.strMeasure4 ? name.strMeasure4 : " "} </span>
-					</div>
-					<div className='drankIngDiv'>
-						<span>{name.strIngredient5 ? name.strIngredient5 : " "}</span>
-						<span>{name.strMeasure5 ? name.strMeasure5 : " "} </span>
-					</div>
-					<div className='drankIngDiv'>
-						<span>{name.strIngredient6 ? name.strIngredient6 : " "}</span>
-						<span>{name.strMeasure6 ? name.strMeasure6 : " "} </span>
-					</div>
+						<div className='drankIngDiv'>
+							<span>{name.strIngredient1 ? name.strIngredient1 : " "}</span>
+							<span>{name.strMeasure1 ? name.strMeasure1 : " "} </span>
+						</div>
+						<div className='drankIngDiv'>
+							<span>{name.strIngredient2 ? name.strIngredient2 : " "}</span>
+							<span>{name.strMeasure2 ? name.strMeasure2 : " "} </span>
+						</div>
+						<div className='drankIngDiv'>
+							<span>{name.strIngredient3 ? name.strIngredient3 : " "}</span>
+							<span>{name.strMeasure3 ? name.strMeasure3 : " "} </span>
+						</div>
+						<div className='drankIngDiv'>
+							<span>{name.strIngredient4 ? name.strIngredient4 : " "}</span>
+							<span>{name.strMeasure4 ? name.strMeasure4 : " "} </span>
+						</div>
+						<div className='drankIngDiv'>
+							<span>{name.strIngredient5 ? name.strIngredient5 : " "}</span>
+							<span>{name.strMeasure5 ? name.strMeasure5 : " "} </span>
+						</div>
+						<div className='drankIngDiv'>
+							<span>{name.strIngredient6 ? name.strIngredient6 : " "}</span>
+							<span>{name.strMeasure6 ? name.strMeasure6 : " "} </span>
+						</div>
 
-					<p>
-						<strong>Insturctions:</strong> {name.strInstructions}
-					</p>
+						<p>
+							<strong>Insturctions:</strong> {name.strInstructions}
+						</p>
+					</div>
 				</div>
 			</div>
 		);
